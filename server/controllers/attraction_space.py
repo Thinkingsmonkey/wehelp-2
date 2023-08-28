3484c05e4171d3789970f3772e3a4ba0ee24324f
neal <neal9918@gmail.com>
2023-08-29 01:13:45 +0800

    create first api

--- server/controllers/attraction_space.py
+++ server/controllers/attraction_space.py
@@ -1,4 +1,4 @@
-from flask_restx import Resource, Namespace
+from flask_restx import Resource, Namespace, reqparse
 from ..extensions import db
 from ..models.db_models import Attraction, AttractionImg
 from ..models.api_models import *
@@ -8,7 +8,18 @@ acttraction_space = Namespace("api/")
 @acttraction_space.route("/acttractions")
 class ActtractionsAPI(Resource):
     
-    # @acttraction_space.marshal_with(attractions_output_model)
-    @acttraction_space.expect(acttractions_input_model)
-    def post(self):
+    @acttraction_space.marshal_with(attractions_output_model)
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("page", type=int, required=True, location="args")
+        parser.add_argument("keyword", type=str, location="args")
+        args = parser.parse_args()
+        
+        page = args["page"]
+        keyword = args["keyword"]
+        
+        page = args["page"]
+        keyword = args["keyword"]
+        attractions = Attraction.query.all()
+        print(attractions)
         return {"ok": True}, 200
